efe45f8b72b511bbcbb57a87c25ab18f8920bf04
svarost <sir.shvetsov@gmail.com>
2021-02-23 17:33:07 +0300

    00

--- src/main/java/level_1Lesson_3/HomeTaskPractice.java
+++ src/main/java/level_1Lesson_3/HomeTaskPractice.java
@@ -10,11 +10,9 @@ public class HomeTaskPractice {
         while (true) {
             guessNumber();
             System.out.println();
-            System.out.println("Повторить игру еще раз? 1- да / 0 - нет.");
+            System.out.println("Повторить игру еще раз? 1 - да / 0 - нет.");
             int answer = sc.nextInt();
-            if (answer == 1) {
-                continue;
-            } else {
+            if (answer == 0) {
                 break;
             }
         }
@@ -23,9 +21,18 @@ public class HomeTaskPractice {
 
     public static void guessNumber() {
         int randomNum = (int) (Math.random() * 10);
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Осталось " + (3 - i) + " попытки. Введите загаданное число: ");
-            int x = sc.nextInt();
+        int x;
+        int count = 0;
+        for (int i = 0;; i++) {
+            System.out.println("Осталось " + (3 - count) + " попытки. Введите загаданное число (от 0 до 9): ");
+            if (sc.hasNextInt()) {
+                x = sc.nextInt();
+                count ++;
+            } else  {
+                sc.nextLine();
+                continue;
+            }
+            sc.nextLine();
             if (x == randomNum) {
                 System.out.println("Поздравляю! Вы угадали!");
                 break;
@@ -34,8 +41,9 @@ public class HomeTaskPractice {
             } else {
                 System.out.println("Загаданное число меньше введенного.");
             }
-            if (i == 2) {
+            if (count == 3) {
                 System.out.println("Вы проиграли.");
+                break;
             }
         }
     }
